c02dcd07e4598381610df6863aa41b89f109b2e2
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-03 08:47:05 -0300

    ex010

---
+++ ex010/main.js
@@ -0,0 +1,14 @@
+let saida = document.querySelector('.saida');
+
+function acao1() {
+  saida.innerHTML += '<p>Clicou no primeiro botão</p>';
+}
+function acao2() {
+  saida.innerHTML += '<p>Clicou no segundo botão</p>';
+}
+function acao3() {
+  saida.innerHTML += '<p>Clicou no terceiro botão</p>';
+}
+function acao4() {
+  saida.innerHTML += '<p>Clicou no quarto botão</p>';
+}
